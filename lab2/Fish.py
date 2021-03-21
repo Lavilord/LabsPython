4fe25bee9f824fbbefe1bad8f8d47df3734fff4c
Lavilord <74894820+Lavilord@users.noreply.github.com>
2021-03-21 20:21:53 +0200

    Python lab4 Borylo

--- lab2/Fish.py
+++ lab2/Fish.py
@@ -1,17 +1,18 @@
 from Animal import Animal
-from Water import Water
 
 
 class Fish(Animal):
-    def __init__(self, type_of_water: Water, weight_in_grams, price, origin_country, is_predator, eats_in_grams):
+    def __init__(self, type_of_water: str, weight_in_grams, price, origin_country, is_predator, eats_in_grams):
         self.type_of_water = type_of_water
         Animal.__init__(self, weight_in_grams, price, origin_country, is_predator, eats_in_grams)
 
 
-class Shark(Animal):
-    def __init__(self, is_dangerous_for_human, type_of_water: Water, weight_in_grams, price, origin_country, is_predator, eats_in_grams):
+class Shark(Fish):
+    def __init__(self, is_dangerous_for_human: bool, type_of_water, weight_in_grams, price, origin_country,
+                 is_predator, eats_in_grams):
         self.is_dangerous_for_human = is_dangerous_for_human
-        Fish.__init__(self, type_of_water, weight_in_grams, price, origin_country, is_predator , eats_in_grams)
+        Fish.__init__(self, type_of_water, weight_in_grams, price, origin_country, is_predator, eats_in_grams)
 
     def __repr__(self):
-        return 'Shark: price: {self.price}, is predator: {self.is_predator}, eats in grams: {self.eats_in_grams} '.format(self=self)
+        return 'Shark: price: {self.price}, is predator: {self.is_predator}, eats in grams: {self.eats_in_grams}\n '\
+            .format(self=self)
--- lab2/Reptile.py
+++ lab2/Reptile.py
@@ -2,24 +2,30 @@ from Animal import Animal
 
 
 class Reptile(Animal):
-    def __init__(self, length_in_centimeters, weight_in_grams, price, origin_country, is_predator, eats_in_grams):
+    def __init__(self, length_in_centimeters: int, weight_in_grams, price, origin_country, is_predator, eats_in_grams):
         self.length_in_centimeters = length_in_centimeters
-        Animal.__init__(self, weight_in_grams, price, origin_country, is_predator , eats_in_grams )
+        Animal.__init__(self, weight_in_grams, price, origin_country, is_predator, eats_in_grams)
 
 
 class Alligator(Reptile):
-    def __init__(self, is_trained, length_in_centimeters, weight_in_grams, price, origin_country, is_predator, eats_in_grams):
+    def __init__(self, is_trained: bool, length_in_centimeters, weight_in_grams, price, origin_country, is_predator,
+                 eats_in_grams):
         self.is_trained = is_trained
-        Reptile.__init__(self, length_in_centimeters, weight_in_grams, price, origin_country, is_predator, eats_in_grams)
+        Reptile.__init__(self, length_in_centimeters, weight_in_grams, price, origin_country, is_predator,
+                         eats_in_grams)
 
     def __repr__(self):
-        return 'Alligator: price: {self.price}, is predator: {self.is_predator}, eats in grams: {self.eats_in_grams} '.format(self=self)
+        return 'Alligator: price: {self.price}, is predator: {self.is_predator}, eats in grams: {self.eats_in_grams}\n'\
+            .format(self=self)
 
 
 class Snake(Reptile):
-    def __init__(self,is_poisonous, length_in_centimeters, weight_in_grams, price, origin_country, is_predator, eats_in_grams):
+    def __init__(self, is_poisonous: bool, length_in_centimeters, weight_in_grams, price, origin_country, is_predator,
+                 eats_in_grams):
         self.is_poisonous = is_poisonous
-        Reptile.__init__(self, length_in_centimeters, weight_in_grams, price, origin_country, is_predator, eats_in_grams )
+        Reptile.__init__(self, length_in_centimeters, weight_in_grams, price, origin_country, is_predator,
+                         eats_in_grams)
 
     def __repr__(self):
-        return 'Snake: price: {self.price}, is predator: {self.is_predator}, eats in grams: {self.eats_in_grams} '.format(self=self)
+        return 'Snake: price: {self.price}, is predator: {self.is_predator}, eats in grams: {self.eats_in_grams}\n '\
+            .format(self=self)
